fc086fe2c87abbd196bdeca387d949f21f1a9522
M0nday0001 <andrey.kovpak.01@gmai.com>
2022-12-19 17:43:01 +0200

    experiments with slides

--- script.js
+++ script.js
@@ -12,5 +12,30 @@ new Swiper('.swiper-container', {
     slidesPerView: 1,
     watchOverflow: true,
     spaceBetween: 120,
+    autoplay: {
+        delay: 1000,
+        stopOnLastSlide: true,
+        disableOnInteraction: false,
+    },
+    speed: 3000,
+    // freeMode: true, // можно скроллить как ленту без фиксации на блоке 
+    loop: true, // бесконечное прокручивание
+    // initialSlide: 1, //дефолт второй слайд ( по индексу первый )
+    // centeredSlides: true,
     // slideToClickedSlide: true,
+
+    // direction: 'vertical',
+
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true,
+    // },
+
+    // effect: 'flip',
+    // flipEffect: {
+    //     slideShadows: true,
+    //     limitRotation: true,
+    // },
+
+
 });
